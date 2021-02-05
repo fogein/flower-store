e1a877c7c588c2d7c30ff6717869c464e5ed2396
fogein <77330295+fogein@users.noreply.github.com>
2021-02-06 01:05:31 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+    const flowersSlider = new Swiper('.flowers-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:6,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+      breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+    
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+    
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 6,
+  
+      }
+    }
+  });
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+
+$("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+      opts : {
+        caption : 'First caption',
+        thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+      }
+    },
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+  
+
+});
+
+
+});
+$(document).ready(function () {
+  $("#review-2").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : ('../img/123.jpg'),
+      opts : {
+        caption : 'First caption',
+        thumb   : '../img/123.jpg'
+      }
+    },
+    {
+      src  : '../img/image-review-1.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : '../img/image-review-1.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+  
+
+});
+});
